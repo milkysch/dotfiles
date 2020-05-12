7162561764f11a29f1f050ff3a11fc7ee2ea6022
milkysch <milkysch@gmail.com>
2020-05-12 18:09:50 +0200

    2020-05-12 update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,5 +9,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top -c ~/.config/polybar/config-top.ini &
-polybar bottom -c ~/.config/polybar/config-bottom.ini &
+#polybar top -c ~/.config/polybar/config-top.ini &
+#polybar bottom -c ~/.config/polybar/config-bottom.ini &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar top -c ~/.config/polybar/config-top.ini &
+    MONITOR=$m polybar bottom -c ~/.config/polybar/config-bottom.ini &
+  done
+else
+  polybar top -c ~/.config/polybar/config-top.ini &
+  polybar bottom -c ~/.config/polybar/config-bottom.ini &
+fi
--- scripts/playclipboard.sh
+++ scripts/playclipboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CLIPBOARD=$(xsel --clipboard)
-/home/milky/scripts/webview.sh "${CLIPBOARD/\&/%26}"
+/home/renne/scripts/webview.sh "${CLIPBOARD/\&/%26}"
 exit 0
